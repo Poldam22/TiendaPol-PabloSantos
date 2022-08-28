5c1f4f93a6f9702b91a25d3a968e3348b7325798
Pablo Santos <pablo.da.santos@gmail.com>
2022-08-28 07:23:27 -0300

    Desafio clase 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola a todos!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Comisión 38080
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+      <div className="App">
+      </div>
+    </>
   );
 }
 
